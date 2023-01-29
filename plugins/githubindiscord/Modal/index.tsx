f21eb9609fbc1f9fd515b6201258927c1c4c46b3
pog who? <54505527+E-boi@users.noreply.github.com>
2023-01-28 22:55:05 -0500

    open modal more smoothly

--- plugins/githubindiscord/Modal/index.tsx
+++ plugins/githubindiscord/Modal/index.tsx
@@ -50,128 +50,127 @@ const GithubModal: FC<ModalProps & { tab: string }> = ({ tab, ...props }) => {
 
   const Tab = tabs.find(({ title }) => title === currentTab);
   return (
-    <ThemeProvider
-      colorMode={(webpack.getByProps("theme")?.theme as "dark" | "light") || "auto"}
-      theme={theme}
-      nightScheme={pluginSettings.get("darkTheme", "dark_discord")}
-      dayScheme={pluginSettings.get("lightTheme", "light_discord")}>
-      <BaseStyles bg="canvas.default">
-        <ModalRoot {...props} className="githubModel">
-          <ModalHeader>
-            <Box className="repo-info">
-              <Text className="repo-path">
-                {repo?.repo.visibility === "private" ? (
-                  <LockIcon className="repo-visibility-icon" size={16} />
-                ) : (
-                  <RepoIcon className="repo-visibility-icon" size={16} />
-                )}
-                <Breadcrumbs>
-                  <Breadcrumbs.Item
-                    href={repo?.repo.owner.html_url}
-                    className={textClasses?.["heading-lg/normal"]}
-                    target="_blank">
-                    {repo?.repo?.owner.login}
-                  </Breadcrumbs.Item>
-                  <Breadcrumbs.Item
-                    href={repo?.repo.html_url}
-                    className={textClasses?.["heading-lg/medium"]}
-                    target="_blank">
-                    {repo?.repo.name}
-                  </Breadcrumbs.Item>
-                </Breadcrumbs>
-                <Label className="visibility">{repo?.repo.visibility}</Label>
-              </Text>
-
-              <div className={[textClasses?.["heading-sm/medium"], "extlink-buttons"].join(" ")}>
-                <ButtonGroup sx={{ display: "flex" }}>
-                  <Button leadingIcon={StarIcon} verticalAlign={"middle"} sx={{ mr: 1 }}>
-                    Stars
-                    <Button.Counter>
-                      {abbreviateNumber(repo?.repo.stargazers_count ?? 0) as unknown as number}
-                    </Button.Counter>
-                  </Button>
-                  <Button>
-                    <a
-                      className="starLink"
-                      href={`${repo?.repo.html_url}/stargazers`}
-                      target="_blank">
-                      <LinkExternalIcon size={16} />
-                    </a>
-                  </Button>
-                </ButtonGroup>
-                <ButtonGroup sx={{ display: "flex" }}>
-                  <Button leadingIcon={RepoForkedIcon} verticalAlign={"middle"} sx={{ mr: 1 }}>
-                    Fork
-                    <Button.Counter>
-                      {abbreviateNumber(repo?.repo.forks_count ?? 0) as unknown as number}
-                    </Button.Counter>
-                  </Button>
-                  <Button>
-                    <a
-                      className="forklink"
-                      href={`${repo?.repo.html_url}/network/members`}
-                      target="_blank">
-                      <LinkExternalIcon size={16} />
-                    </a>
-                  </Button>
-                </ButtonGroup>
-              </div>
-            </Box>
-            <UnderlineNav aria-label="Repository">
-              {tabs.map(({ title, icon }) => (
-                <UnderlineNav.Item
-                  icon={icon}
-                  aria-current={title === currentTab}
-                  onSelect={() => setTab(title)}
-                  counter={
-                    title === "Issues"
-                      ? abbreviateNumber(repo?.issues.data?.totalOpen ?? 0)
-                      : title === "Pull Requests"
-                      ? abbreviateNumber(repo?.prs.data?.totalOpen ?? 0)
-                      : undefined
-                  }>
-                  {title}
-                </UnderlineNav.Item>
-              ))}
-            </UnderlineNav>
-          </ModalHeader>
-          <ModalContent>
-            {status === "loading" ? (
-              <Spinner>Fetching Repository Contents...</Spinner>
+    <ModalRoot {...props} className="githubModel">
+      <ModalHeader>
+        <Box className="repo-info">
+          <Text className="repo-path">
+            {repo?.repo.visibility === "private" ? (
+              <LockIcon className="repo-visibility-icon" size={16} />
             ) : (
-              Tab && repo && repo.tree && <Tab.component />
+              <RepoIcon className="repo-visibility-icon" size={16} />
             )}
-          </ModalContent>
-          <ModalFooter>
-            <ButtonGroup>
-              <Button
-                onClick={() =>
-                  refetch(
-                    {
-                      issues: { state: "open" },
-                      prs: { state: "open" },
-                      branch: repo?.currentBranch.name,
-                    },
-                    true,
-                  )
-                }>
-                Refetch Repository
+            <Breadcrumbs>
+              <Breadcrumbs.Item
+                href={repo?.repo.owner.html_url}
+                className={textClasses?.["heading-lg/normal"]}
+                target="_blank">
+                {repo?.repo?.owner.login}
+              </Breadcrumbs.Item>
+              <Breadcrumbs.Item
+                href={repo?.repo.html_url}
+                className={textClasses?.["heading-lg/medium"]}
+                target="_blank">
+                {repo?.repo.name}
+              </Breadcrumbs.Item>
+            </Breadcrumbs>
+            <Label className="visibility">{repo?.repo.visibility}</Label>
+          </Text>
+
+          <div className={[textClasses?.["heading-sm/medium"], "extlink-buttons"].join(" ")}>
+            <ButtonGroup sx={{ display: "flex" }}>
+              <Button leadingIcon={StarIcon} verticalAlign={"middle"} sx={{ mr: 1 }}>
+                Stars
+                <Button.Counter>
+                  {abbreviateNumber(repo?.repo.stargazers_count ?? 0) as unknown as number}
+                </Button.Counter>
               </Button>
-              <Button onClick={openSettingsModal}>Open Settings</Button>
+              <Button>
+                <a className="starLink" href={`${repo?.repo.html_url}/stargazers`} target="_blank">
+                  <LinkExternalIcon size={16} />
+                </a>
+              </Button>
             </ButtonGroup>
-          </ModalFooter>
-        </ModalRoot>
-      </BaseStyles>
-    </ThemeProvider>
+            <ButtonGroup sx={{ display: "flex" }}>
+              <Button leadingIcon={RepoForkedIcon} verticalAlign={"middle"} sx={{ mr: 1 }}>
+                Fork
+                <Button.Counter>
+                  {abbreviateNumber(repo?.repo.forks_count ?? 0) as unknown as number}
+                </Button.Counter>
+              </Button>
+              <Button>
+                <a
+                  className="forklink"
+                  href={`${repo?.repo.html_url}/network/members`}
+                  target="_blank">
+                  <LinkExternalIcon size={16} />
+                </a>
+              </Button>
+            </ButtonGroup>
+          </div>
+        </Box>
+        <UnderlineNav aria-label="Repository">
+          {tabs.map(({ title, icon }) => (
+            <UnderlineNav.Item
+              icon={icon}
+              aria-current={title === currentTab}
+              onSelect={() => setTab(title)}
+              counter={
+                title === "Issues"
+                  ? abbreviateNumber(repo?.issues.data?.totalOpen ?? 0)
+                  : title === "Pull Requests"
+                  ? abbreviateNumber(repo?.prs.data?.totalOpen ?? 0)
+                  : undefined
+              }>
+              {title}
+            </UnderlineNav.Item>
+          ))}
+        </UnderlineNav>
+      </ModalHeader>
+      <ModalContent>
+        {status === "loading" ? (
+          <Spinner>Fetching Repository Contents...</Spinner>
+        ) : (
+          Tab && repo && repo.tree && <Tab.component />
+        )}
+      </ModalContent>
+      <ModalFooter>
+        <ButtonGroup>
+          <Button
+            onClick={() =>
+              refetch(
+                {
+                  issues: { state: "open" },
+                  prs: { state: "open" },
+                  branch: repo?.currentBranch.name,
+                },
+                true,
+              )
+            }>
+            Refetch Repository
+          </Button>
+          <Button onClick={openSettingsModal}>Open Settings</Button>
+        </ButtonGroup>
+      </ModalFooter>
+    </ModalRoot>
   );
 };
 
 export function openGithubModal(url: string, tab: string) {
   common.modal.openModal((props) => (
     <ErrorBoundary modalProps={props}>
-      <Provider query={{ issues: { state: "open" }, prs: { state: "open" } }} url={url}>
-        <GithubModal {...props} tab={tab} />
-      </Provider>
+      <ThemeProvider
+        colorMode={(webpack.getByProps("theme")?.theme as "dark" | "light") || "auto"}
+        theme={theme}
+        nightScheme={pluginSettings.get("darkTheme", "dark_discord")}
+        dayScheme={pluginSettings.get("lightTheme", "light_discord")}>
+        <BaseStyles>
+          <Provider query={{ issues: { state: "open" }, prs: { state: "open" } }} url={url}>
+            <Box sx={{ ".githubModel": { bg: "canvas.default" } }}>
+              <GithubModal {...props} tab={tab} />
+            </Box>
+          </Provider>
+        </BaseStyles>
+      </ThemeProvider>
     </ErrorBoundary>
   ));
 }
