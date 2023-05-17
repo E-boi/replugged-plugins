8e5a8beb91f42eb0e6ec2bd7b6adfcbcc4205a4a
pog who? <54505527+E-boi@users.noreply.github.com>
2023-05-17 19:19:41 -0400

    fix rpc

--- plugins/customrpc/components/UserActivities.tsx
+++ plugins/customrpc/components/UserActivities.tsx
@@ -1,6 +1,6 @@
 import { webpack } from "replugged";
 import { AnyFunction, ObjectExports } from "replugged/dist/types";
-import { Scroller, UserActivity } from ".";
+import { UserActivity } from ".";
 
 const useStateFromStoreRaw = await webpack.waitForModule(
   webpack.filters.bySource("useStateFromStores"),
@@ -9,7 +9,7 @@ const useStateFromStore = webpack.getFunctionBySource(
   useStateFromStoreRaw as ObjectExports,
   "useStateFromStores",
 );
-const ActivityStore = webpack.getByProps("getActivities");
+const ActivityStore = webpack.getByProps("getLocalPresence");
 const user = webpack.getByProps("getCurrentUser");
 
 const classes = {
@@ -21,26 +21,21 @@ export default () => {
     (ActivityStore!.getActivities as AnyFunction)(),
   ) as unknown[];
 
-  if (!Scroller) return null;
-
   return (
     <div className={`${classes.profileColors} rprpc-activities`}>
-      {/* eslint-disable-next-line new-cap */}
-      {Scroller({
-        children: activities?.map(
-          (a) =>
-            UserActivity && (
-              <UserActivity
-                activity={a}
-                className="rprpc-activity"
-                source="Profile Modal"
-                type="ProfileV2"
-                useStoreStream={false}
-                user={(user?.getCurrentUser as AnyFunction)()}
-              />
-            ),
-        ),
-      })}
+      {activities?.map(
+        (a) =>
+          UserActivity && (
+            <UserActivity
+              activity={a}
+              className="rprpc-activity"
+              source="Profile Modal"
+              type="ProfileV2"
+              useStoreStream={false}
+              user={(user?.getCurrentUser as AnyFunction)()}
+            />
+          ),
+      )}
     </div>
   );
 };
